cbd270867faab77069781ffec59ae6881cf13d77
wjch-krl <wjch.krl@gmail.com>
2015-10-23 17:12:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
   name='pgCassandra',
   version='0.9.2',
   author='Wojciech Krol',
+  link = 'https://github.com/wjch-krl/pgCassandra/',
   license='Postgresql',
   packages=['pgCassandra']
 )
