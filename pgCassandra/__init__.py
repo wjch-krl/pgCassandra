367e51b385387be80c602cbee8afe17a685c38c4
Wojciech Król <wjch.krl@gmail.com>
2015-10-26 13:09:23 +0100

    Query generation fixes

--- pgCassandra/__init__.py
+++ pgCassandra/__init__.py
@@ -1,88 +1,96 @@
 from multicorn import ForeignDataWrapper
-from multicorn.utils import log_to_postgres, ERROR, WARNING, DEBUG
+from multicorn.utils import log_to_postgres, ERROR, WARNING, DEBUG, INFO
 from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+from collections import defaultdict
 
 class CassandraFDW(ForeignDataWrapper):
-    IDX_QUERY_COST = 1000
-    CLUSTERING_KEY_QUERY_COST = 100
-    PARTITION_KEY_QUERY_COST = 10
-    PRIMARY_KEY_QUERY_COST = 1
-
-    def __init__(self, options, columns):
-        super(CassandraFDW, self).__init__(options, columns)
-        # get options
-        if "hosts" not in options:
-            log_to_postgres("The hosts parameter is needed, setting to localhost.", WARNING)
-        hosts = options.get("hosts", "localhost").split(",");
-        if "port" not in options:
-            log_to_postgres("The port parameter is needed, setting to  9042.", WARNING)
-        self.port = options.get("port", "9042")
-        if (("keyspace" not in options) or ("columnfamily" not in options)) and ("query" not in options):
-            log_to_postgres("Either query or columnfamily parameter is required.", ERROR)
-        self.columnfamily = options.get("columnfamily", None)
-        self.keyspace = options.get("keyspace", None)
-        self.query = options.get("query", None)
-        self.limit = options.get("limit", None)
-        timeout =  options.get("timeout", None)
-        
-        # Cassandra connection init
-        self.cluster =  Cluster(hosts)
-        self.session = self.cluster.connect()
-        if (timeout)
-            self.session.default_timeout = timeout
-        # Get querable columns
-        tableInfoQuery = "select column_name,type,index_type from system.schema_columns" +
-            " where keyspace_name = '{0}' and columnfamily_name = '{1}'".format(keyspace,columnfamily)
-        result = self.session.execute(statement)
-        self.queryableColumns = {}
-        for row in result:
-            columnName = row["column_name"]
-            columnKeyType = row["type"]
-            columnIdxType = row["index_type"]
-            if (columnKeyType == "regular") and (columnIdxType == None):
-                continue
-            self.queryableColumns[columnName] = keyTypeToCost(columnKeyType,columnIdxType)    
-        
-    def keyTypeToCost(columnKeyType,columnIdxType):
-        if (columnKeyType == "clustering_key"):
-            return CLUSTERING_KEY_QUERY_COST
-        if (columnKeyType == "partition_key"):
-            return PARTITION_KEY_QUERY_COST
-        return IDX_QUERY_COST
-        
-    def execute(self, quals, columns):
-        if (self.query):
-            statement = self.query
-        else:
-            statement = "SELECT {0} FROM {1}".format(",".join(columns), self.columnfamily);
-        # TODO don't query when clustering key is queried and partion key isn't
-        isWhere = None
-        for qual in quals:
-            if (qual.operator == "="):
-                if (qual.field_name in self.queryableColumns):
-                    if isWhere:
-                        statement += " AND {0} = {1} ".format(qual.field_name ,qual.value)
-                    else:
-                        statement += " WHERE {0} = {1} ".format(qual.field_name ,qual.value)
-                        isWhere = 1
-        if (self.limit):
-            staement += " limit {0}".format(limit);
-        log_to_postgres("CQL query: {1}".format(unicode(statement)), WARNING)
-        
-        result = self.session.execute(statement)
-        for row in result:
-            line = {}
-            idx = 0
-            for column_name in columns:
-                line[column_name] = row[idx++]
-            yield line
-            
-    # def qualValueToString(qual,columns):
-    #     if qual.     
-            
-    def get_path_keys(self):
-        return [(obj.key, obj.value) for obj in self.queryableColumns]
-        
-        
+	IDX_QUERY_COST = 1000
+	CLUSTERING_KEY_QUERY_COST = 100
+	PARTITION_KEY_QUERY_COST = 1
+	PRIMARY_KEY_QUERY_COST = 1
+	
+	def keyTypeToCost(self,columnKeyType,columnIdxType):
+		if (columnKeyType == "clustering_key"):
+			return self.CLUSTERING_KEY_QUERY_COST
+		if (columnKeyType == "partition_key"):
+			return self.PARTITION_KEY_QUERY_COST
+		return self.IDX_QUERY_COST
+	
+	def qualValueToString(self, qual):
+		if ((type(qual.value) is str) or (type(qual.value) is unicode)):
+			return "'{0}'".format(qual.value)
+		return "{0}".format(qual.value)
+	
+	def __init__(self, options, columns):
+		super(CassandraFDW, self).__init__(options, columns)
+		# get options
+		if "hosts" not in options:
+			log_to_postgres("The hosts parameter is needed, setting to localhost.", WARNING)
+		hosts = options.get("hosts", "localhost").split(",");
+		if "port" not in options:
+			log_to_postgres("The port parameter is needed, setting to 9042.", WARNING)
+		self.port = options.get("port", "9042")
+		if (("keyspace" not in options) or ("columnfamily" not in options)) and ("query" not in options):
+			log_to_postgres("Either query or columnfamily and keyspace parameter is required.", ERROR)
+		self.columnfamily = options.get("columnfamily", None)
+		self.keyspace = options.get("keyspace", None)
+		self.query = options.get("query", None)
+		self.limit = options.get("limit", None)
+		timeout =  options.get("timeout", None)
+		
+		# Cassandra connection init
+		self.cluster =  Cluster(hosts)
+		self.session = self.cluster.connect()
+		if (timeout):
+			self.session.default_timeout = timeout
+		# Get querable columns
+		tableInfoQuery = "select column_name,type,index_type from system.schema_columns where keyspace_name = '{0}' and columnfamily_name = '{1}'".format(self.keyspace,self.columnfamily)
+		result = self.session.execute(tableInfoQuery)
+		self.queryableColumns = {}
+		for row in result:
+			columnName = row[0]
+			columnKeyType = row[1]
+			columnIdxType = row[2]
+			if (columnKeyType == "regular") and (columnIdxType == None):
+				continue
+			self.queryableColumns[columnName] = self.keyTypeToCost(columnKeyType,columnIdxType)	
+	
+	
+	def execute(self, quals, columns):
+		statement = ""
+		if (self.query):
+			statement = self.query
+		else:
+			statement = "SELECT {0} FROM {1}.{2}".format(",".join(columns), self.keyspace, self.columnfamily);
+		# TODO don't query when clustering key is queried and partion key isn't
+			isWhere = None
+			for qual in quals:
+				if (qual.operator == "="):
+					if (qual.field_name in self.queryableColumns):
+						if isWhere:
+							statement += " AND {0} = {1} ".format(qual.field_name,self.qualValueToString(qual))
+						else:
+							statement += " WHERE {0} = {1} ".format(qual.field_name,self.qualValueToString(qual))
+							isWhere = 1
+		if (self.limit):
+			statement += " limit {0}".format(limit);
+		#log_to_postgres("CQL query: {0}".format(statement), DEBUG)
+		
+		result = self.session.execute(statement)
+		for row in result:
+			line = {}
+			idx = 0
+			for column_name in columns:
+				line[column_name] = row[idx]
+				idx = idx + 1
+			yield line
+			 
+			
+	def get_path_keys(self):
+		s = [(v,k.encode('ascii','ignore')) for k,v in self.queryableColumns.iteritems()]
+		d = defaultdict(list)
+		for k, v in s:
+			d[k].append(v)
+		return [(tuple(v),k) for k,v in d.items()]
